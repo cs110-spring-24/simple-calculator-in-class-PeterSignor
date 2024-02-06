7d6ab369735e1800ae5d7ef416e2a5c27d5c61b1
PeterSignor <petereysig@gmail.com>
2024-02-05 16:16:12 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,29 @@
+number1 = int(input("enter the first number: "))
+operator = input(" enter an operator: ")
+number2 = int(input("enter a second number: "))
+if operator == "+":
+	total = number1 + number2
+	print(f"{number1} + {number2} = {total}")
+elif operator == "-":
+	total = number1 - number2
+	print(f"{number1} - {number2} = {total}")
 
+elif operator == "*":
+	total = number1 * number2
+	print(f"{number1} * {number2} = {total}")	
+
+elif operator == "/":
+	total = number1 / number2
+	print(f"{number1} / {number2} = {total}")	
+
+elif operator == "**":
+	total = number1 ** number2
+	print(f"{number1} ** {number2} = {total}")
+
+elif operator == "//":
+	total = number1 // number2
+	print(f"{number1} // {number2} = {total}")	
+
+elif operator == "%":
+	total = number1 % number2
+	print(f"{number1} % {number2} = {total}")	
